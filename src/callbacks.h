8a5f1bcd0ae2bd053ec5f0df3c5c8f69a8d511f3
jesser-007 <jesser.sallemi@esprit.tn>
2023-11-14 16:06:50 +0100

    Add the build of project c

---
+++ src/callbacks.h
@@ -0,0 +1,222 @@
+#include <gtk/gtk.h>
+
+
+void
+on_button1_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button3_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button4_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button5_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button6_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button2_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button7_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button9_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_typeSangRareBouttonDon_clicked      (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_coubeSangBouttonDon_clicked         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_modifierBouttonDon_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_supprimerBouttonDon_clicked         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_deconnexionBouttonDon_clicked       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_confirmerBouttonADon_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_confirmerBouttonSDon_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_confirmerBouttonMDon_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonAccesFicheDonneur_clicked     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_CreationFicheDonneur_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button52_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonAffichageFicheDonneur1_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button28_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSupprimerFicheDonneur_clicked (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonRechercheFicheDonneur2_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonRetourManipVersAcces_clicked  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonRetourRechercheVersAcces_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button33_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonNonSupprimerFicheDonneur_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button35_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_supp_ets_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_afichage_ets_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_modifier_ets_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_ajouter_ets_clicked         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_back_ets_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_ajoutermodif2_ets_clicked    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_oksupprission_ets_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonmodifier4_ets_clicked         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_back_ets_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_back_sg_clicked              (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_supp_sg_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_ajouter_sg_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_affichage_sg_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_modifier_sg_clicked         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button51_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_supprimer1_rdv_clicked      (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_historique1_rdv_clicked     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_deco1_rdv_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_ajouter1_rdv_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_modifier1_rdv_clicked       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_valider3_rdv_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_boutton_valider4_rdv_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonChoix_uts_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonChoix_ets_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonChoix_deconnexion_clicked     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonconfM_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
